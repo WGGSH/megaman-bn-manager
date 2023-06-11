973728fa0d121fa15d8de3b41973036ef033e6fe
nao <naokipgt@gmail.com>
2023-06-11 12:13:14 +0900

    環境構築

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  router: {
+    base: "/megaman-bn-manager/",
+  },
+  app: {
+    cdnURL: "/megaman-bn-manager",
+  },
+  generate: {
+    dir: "docs",
+  },
+});
