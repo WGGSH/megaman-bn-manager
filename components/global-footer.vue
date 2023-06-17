d4e2527c6adca01157353d3e88e45339ada93483
nao <naokipgt@gmail.com>
2023-06-17 18:57:03 +0900

    フッター作成

--- components/global-footer.vue
+++ components/global-footer.vue
@@ -1,5 +1,46 @@
 <template>
-  <v-footer color="secondary" app>
-    footer
+  <v-footer color="secondary" app border class="pa-0 pb-4">
+    <v-card
+      elevation="0"
+      tile
+      width="100%"
+      class="text-center"
+      color="secondary"
+    >
+      <v-card-text class="pa-2">
+        <v-btn
+          v-for="menu in footerMenus"
+          :key="menu.icon"
+          class="mx-auto"
+          variant="plain"
+        >
+          <v-icon size="36px">
+            {{ menu.icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
   </v-footer>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const footerMenus = ref([
+  {
+    icon: 'mdi-home',
+  },
+  {
+    icon: 'mdi-face-man',
+  },
+  {
+    icon: 'mdi-chip',
+  },
+  {
+    icon: 'mdi-credit-card-multiple',
+  },
+  {
+    icon: 'mdi-cog',
+  },
+]);
+</script>
