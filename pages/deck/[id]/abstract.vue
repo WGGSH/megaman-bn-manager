3a977344ce7ad51a8e0d87980b5031cd2e474ac0
nao <naokipgt@gmail.com>
2023-06-20 23:42:20 +0900

    HPメモリ，対応バージョンの表示作成

--- pages/deck/[id]/abstract.vue
+++ pages/deck/[id]/abstract.vue
@@ -4,16 +4,9 @@
     <v-row>
       <h3>Title</h3>
     </v-row>
-    <v-row>
-      <v-text-field v-model="name" />
-    </v-row>
 
     <v-row>
-      <ui-button-accept
-        @click="onClickSave"
-      >
-        保存する
-      </ui-button-accept>
+      <v-text-field v-model="name" />
     </v-row>
 
     <v-row class="mt-8">
@@ -21,9 +14,29 @@
     </v-row>
 
     <v-row>
-      <v-checkbox>
-        aaa
-      </v-checkbox>
+      <v-col v-for="version in versionList" :key="version.key">
+        <v-checkbox v-model="versions" :label="version.name" :value="version.key" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <h3>HPメモリ</h3>
+    </v-row>
+
+    <v-row>
+      <v-text-field v-model="hpMemoryNum" type="number" max="45" min="0" />
+    </v-row>
+
+    <v-row class="mt-8">
+      <v-divider />
+    </v-row>
+
+    <v-row class="mt-8">
+      <ui-button-accept
+        @click="onClickSave"
+      >
+        保存する
+      </ui-button-accept>
     </v-row>
   </v-container>
 </template>
@@ -39,6 +52,19 @@ const route = useRoute();
 const selectedDeck = computed(() => deckManagerStore.selectedDeck);
 
 const name = ref();
+const versions = ref([]);
+const hpMemoryNum = ref(45);
+
+const versionList = [
+  {
+    key: 'G',
+    name: 'グレイガ',
+  },
+  {
+    key: 'F',
+    name: 'ファルザー',
+  },
+];
 
 watch(
   () => selectedDeck.value,
