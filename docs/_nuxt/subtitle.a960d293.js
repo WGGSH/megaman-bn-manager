997c01166ecd0476fb83ffafba0e25adf7359b8e
naoki <naokipgt@gmail.com>
2023-07-22 23:18:10 +0900

    release

---
+++ docs/_nuxt/subtitle.a960d293.js
@@ -0,0 +1,1 @@
+import{a as n,o as s,p as t,b as o}from"./entry.540e0128.js";const c={},r={class:"mb-4"};function a(e,_){return s(),t("h1",r,[o(e.$slots,"default")])}const u=n(c,[["render",a]]),f={},l={class:"my-4"};function d(e,_){return s(),t("h2",l,[o(e.$slots,"default")])}const i=n(f,[["render",d]]);export{u as _,i as a};
