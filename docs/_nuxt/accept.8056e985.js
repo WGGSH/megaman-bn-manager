b298a719d6729b52c4057e74fc7782717e4deb8e
naoki <naokipgt@gmail.com>
2023-07-23 12:02:42 +0900

    release

---
+++ docs/_nuxt/accept.8056e985.js
@@ -0,0 +1,1 @@
+import{a as t,r as e,o as s,c as _,w as c,b as r}from"./entry.1ae8b64b.js";const a={};function l(n,f){const o=e("v-btn");return s(),_(o,null,{default:c(()=>[r(n.$slots,"default")]),_:3})}const m=t(a,[["render",l]]);export{m as _};
